6c0d804dfe2fe586260adcd5529fee84d2cf74b3
Muhammad Sarim <sarimkhan13579@gmail.com>
2025-11-01 00:09:23 -0700

    feat: add Prisma integration with Snippet model and dotenv support

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig, env } from "prisma/config";
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
-});
+// Prevent multiple instances of Prisma Client in development
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prismaInstance = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prismaInstance;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 "use client"
 import React, { useState } from 'react'
+import prismaInstance from '../../prisma.config'
 
 const page = () => {
 
-  const [Title, setTitle] = useState("")
-  const [Snippet, setSnippet] = useState("")
-  const HandleSubmit = (e:React.FormEvent)=>{
-    if(Title === "" || Snippet ===""){
-      alert("fill the fields correctly")
-      return
-    }
-    e.preventDefault()
-    console.log(Title,Snippet)
-    setTitle("")
-    setSnippet("")
+  const HandleSubmit = async (FormData:any) => {
+    "use server"
+    FormData.preventDefault()
+    const Title = FormData.get("title") as string
+    const Snippet = FormData.get("snippet") as string
+    console.log(Title, Snippet)
 
+const snippet = await prismaInstance.snippet.create({
+  data: {
+    title: Title,
+    code: Snippet
+  }
+})
+
+console.log(snippet)
   }
   return (
     <div className='min-h-screen w-screen px-5 py-3'>
@@ -23,17 +27,12 @@ const page = () => {
       <div className='flex flex-col justify-center items-center mt-5 gap-4 p-5'>
         <form className='flex flex-col justify-center items-center mt-5 gap-4 p-5' action="" onSubmit={HandleSubmit}>
           <input 
-          value={Title}
-          onChange={(e)=>{
-setTitle(e.target.value)
-          }}
+        
+          name="title"
           type="text" className='w-fit border border-gray-400 px-4 py-2 rounded'  />
-        <textarea 
-        value={Snippet}
-        onChange={(e)=>{
-          setSnippet(e.target.value)
-        }}
-        className=' border border-gray-400 px-4 py-2 rounded h-64 w-72' name="" id=""></textarea>
+        <textarea
+        name="snippet"
+        className=' border border-gray-400 px-4 py-2 rounded h-64 w-72' id=""></textarea>
         <button className=' cursor-pointer w-fit border bg-black text-amber-50 border-gray-400 px-4 py-2 rounded'>Create</button>
         </form>
       </div>
@@ -44,3 +43,4 @@ setTitle(e.target.value)
 }
 
 export default page
+
